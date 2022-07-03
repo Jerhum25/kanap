43d49227b3949d51d478078b4a3997915f4da762
jerhum <jerhum@msn.com>
2022-07-03 10:21:15 +0200

    modifs panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -5,66 +5,99 @@
 // let nom = localStorage.getItem("name");
 // let price = localStorage.getItem("price");
 // let quantity = localStorage.getItem("quantity");
-let panierVide = [];
-let recup = JSON.parse(localStorage.panier);
+// panierVide.push(recup);
+// // console.log(recup.name)
+// localStorage.setItem("produit", JSON.stringify(panierVide));
 
-console.log(recup)
-panierVide.push(recup);
-console.log(recup.name)
-localStorage.setItem("produit1", JSON.stringify(panierVide));
+// ajoutPanier()
 
-ajoutPanier()
+// function ajoutPanier() {
+
+//     let cart__item = document.querySelector("#cart__items").appendChild(document.createElement("article"));
+//     cart__item.className = "cart__item";
+//     cart__item.id = recup.id;
+//     cart__item.color = JSON.parse(localStorage.getItem("panier")).color;
+
+//     let cart__item__img = cart__item.appendChild(document.createElement("div"));
+//     cart__item__img.className = "cart__item__img";
+
+//     let img = cart__item__img.appendChild(document.createElement("img"));
+//     img.src = JSON.parse(localStorage.getItem("panier")).imgUrl;
+//     img.alt = JSON.parse(localStorage.getItem("panier")).altTxt;
+
+//     let cart__item__content = cart__item.appendChild(document.createElement("div"));
+//     cart__item__content.className = "cart__item__content";
+
+//     let cart__item__content__description = cart__item__content.appendChild(document.createElement("div"));
+//     cart__item__content__description.name = "cart__item__content__description";
+
+//     let h2 = cart__item__content__description.appendChild(document.createElement("h2"));
+//     h2.innerHTML = JSON.stringify(localStorage.getItem("panier").name);
+
+//     let pColor = cart__item__content__description.appendChild(document.createElement("p"));
+//     pColor.innerHTML = JSON.parse(localStorage.getItem("panier")).color;
+
+//     let pPrice = cart__item__content__description.appendChild(document.createElement("p"));
+//     pPrice.innerHTML = JSON.parse(localStorage.getItem("panier")).price + "€";
+
+//     let cart__item__content__settings = cart__item__content.appendChild(document.createElement("div"));
+//     cart__item__content__settings.className = "cart__item__content__settings";
+
+//     let cart__item__content__settings__quantity = cart__item__content__settings.appendChild(document.createElement("div"));
+//     cart__item__content__settings__quantity.className = "cart__item__content__settings__quantity";
+
+//     let pQuantity = cart__item__content__settings__quantity.appendChild(document.createElement("p"));
+//     pQuantity.innerHTML = "Qté : ";
+
+//     let itemQuantity = cart__item__content__settings__quantity.appendChild(document.createElement("input"));
+//     itemQuantity.type = "number"
+//     itemQuantity.className = "itemQuantity";
+//     itemQuantity.name = "itemQuantity";
+//     itemQuantity.setAttribute("min", 1);
+//     itemQuantity.setAttribute("max", 100);
+//     itemQuantity.value = JSON.parse(localStorage.getItem("panier")).quantity;
+
+//     let cart__item__content__settings__delete = cart__item__content__settings__quantity.appendChild(document.createElement("div"));
+//     cart__item__content__settings__delete.className = "cart__item__content__settings__delete";
+
+//     let pDelete = cart__item__content__settings.appendChild(document.createElement("p"));
+//     pDelete.className = "deleteItem";
+//     pDelete.innerHTML = "Supprimer";
+// }
+
+
+let panier = JSON.parse(localStorage.getItem("panier"));
+console.log(panier);
+
 
 function ajoutPanier() {
-
-    let cart__item = document.querySelector("#cart__items").appendChild(document.createElement("article"));
-    cart__item.className = "cart__item";
-    cart__item.id = recup.id;
-    cart__item.color = JSON.parse(localStorage.getItem("panier")).color;
-
-    let cart__item__img = cart__item.appendChild(document.createElement("div"));
-    cart__item__img.className = "cart__item__img";
-
-    let img = cart__item__img.appendChild(document.createElement("img"));
-    img.src = JSON.parse(localStorage.getItem("panier")).imgUrl;
-    img.alt = JSON.parse(localStorage.getItem("panier")).altTxt;
-
-    let cart__item__content = cart__item.appendChild(document.createElement("div"));
-    cart__item__content.className = "cart__item__content";
-
-    let cart__item__content__description = cart__item__content.appendChild(document.createElement("div"));
-    cart__item__content__description.name = "cart__item__content__description";
-
-    let h2 = cart__item__content__description.appendChild(document.createElement("h2"));
-    h2.innerHTML = JSON.stringify(localStorage.getItem("panier").name);
-
-    let pColor = cart__item__content__description.appendChild(document.createElement("p"));
-    pColor.innerHTML = JSON.parse(localStorage.getItem("panier")).color;
-
-    let pPrice = cart__item__content__description.appendChild(document.createElement("p"));
-    pPrice.innerHTML = JSON.parse(localStorage.getItem("panier")).price + "€";
-
-    let cart__item__content__settings = cart__item__content.appendChild(document.createElement("div"));
-    cart__item__content__settings.className = "cart__item__content__settings";
-
-    let cart__item__content__settings__quantity = cart__item__content__settings.appendChild(document.createElement("div"));
-    cart__item__content__settings__quantity.className = "cart__item__content__settings__quantity";
-
-    let pQuantity = cart__item__content__settings__quantity.appendChild(document.createElement("p"));
-    pQuantity.innerHTML = "Qté : ";
-
-    let itemQuantity = cart__item__content__settings__quantity.appendChild(document.createElement("input"));
-    itemQuantity.type = "number"
-    itemQuantity.className = "itemQuantity";
-    itemQuantity.name = "itemQuantity";
-    itemQuantity.setAttribute("min", 1);
-    itemQuantity.setAttribute("max", 100);
-    itemQuantity.value = JSON.parse(localStorage.getItem("panier")).quantity;
-
-    let cart__item__content__settings__delete = cart__item__content__settings__quantity.appendChild(document.createElement("div"));
-    cart__item__content__settings__delete.className = "cart__item__content__settings__delete";
-
-    let pDelete = cart__item__content__settings.appendChild(document.createElement("p"));
-    pDelete.className = "deleteItem";
-    pDelete.innerHTML = "Supprimer";
+  for (let i in panier){
+    document.querySelector("#cart__items").innerHTML = 
+    `<article class="cart__item" data-id="${panier[i].id}" data-color="${panier[i].color}">
+    <div class="cart__item__img">
+      <img src="${panier[i].imageUrl}" alt="${panier[i].altTxt}">
+    </div>
+    <div class="cart__item__content">
+      <div class="cart__item__content__description">
+        <h2>${panier[i].name}</h2>
+        <p>Vert</p>
+        <p>${panier[i].price} €</p>
+      </div>
+      <div class="cart__item__content__settings">
+        <div class="cart__item__content__settings__quantity">
+          <p>Qté : ${panier[i].quantity}</p>
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${panier[i].quantity}">
+        </div>
+        <div class="cart__item__content__settings__delete">
+          <p class="deleteItem">Supprimer</p>
+        </div>
+      </div>
+    </div>
+  </article>`
+  console.log(i);
 }
+}
+
+ajoutPanier();
+
+document.getElementById("totalQuantity").innerHTML = panier.length;
--- front/js/product.js
+++ front/js/product.js
@@ -4,7 +4,6 @@ var parsedUrl = new URL(window.location.href);
 var urlId = parsedUrl.searchParams.get("id");
 // console.log(urlId)
 
-
 fetch(`http://localhost:3000/api/products/${urlId}`)
     .then(data => data.json())
     .then(product => {
@@ -21,8 +20,50 @@ fetch(`http://localhost:3000/api/products/${urlId}`)
                 .appendChild(productColors)
                 .innerHTML = `<option value="${colors}">${colors}</option>`;
         }
+        // addToCart.onclick = (e) => {
+        //     if (colors.value == "null" || colors.value == "") {
+        //         alert("Veuillez choisir une couleur !")
+        //     }
+        //     else if (quantity.value < 1 || quantity.value > 100) {
+        //         alert("Veuillez choisir une quantité entre 1 et 100 !")
+        //     }
+        //     else {
+        //         let articlesPanier = [];
+        //         const save = {};
+        //         save.name = product.name;
+        //         save.id = product._id;
+        //         save.imageUrl = product.imageUrl;
+        //         save.altTxt = product.altTxt;
+        //         save.price = product.price;
+        //         save.description = product.description;
+        //         save.color = colors.value;
+        //         save.quantity = quantity.value;
+        //         articlesPanier.push(save);
+
+        //         localStorage.setItem("panier", JSON.stringify(articlesPanier));
+        //         // localStorage.setItem("id", product._id)
+        //         // localStorage.setItem("imageUrl", product.imageUrl)
+        //         // localStorage.setItem("altTxt", product.altTxt)
+        //         // localStorage.setItem("price", product.price)
+        //         // localStorage.setItem("description", product.description)
+        //         // localStorage.setItem("color", colors.value)
+        //         // localStorage.setItem("quantity", quantity.value)
+        //         console.log(JSON.parse(localStorage.panier));
+        //     }
+        // }
+
+        const quantityProduct = document.querySelector("#quantity");
+
+
+        //Quantité du produit
+        let numberKanap;
+        quantityProduct.addEventListener('change', (e) => {
+            numberKanap = parseInt(e.target.value);
+        })
+
+
         addToCart.onclick = (e) => {
-            if (colors.value == "null" || colors.value == "") {
+            if (colors.value === "null" || colors.value === "") {
                 alert("Veuillez choisir une couleur !")
             }
             else if (quantity.value < 1 || quantity.value > 100) {
@@ -30,28 +71,67 @@ fetch(`http://localhost:3000/api/products/${urlId}`)
             }
             else {
                 let articlesPanier = [];
-                const save = {};
-                save.name = product.name;
-                save.id = product._id;
-                save.imageUrl = product.imageUrl;
-                save.altTxt = product.altTxt;
-                save.price = product.price;
-                save.description = product.description;
-                save.color = colors.value;
-                save.quantity = quantity.value;
-                articlesPanier.push(save);
+                const save = {
+                    name: product.name,
+                    id: product._id,
+                    imageUrl: product.imageUrl,
+                    altTxt: product.altTxt,
+                    price: product.price,
+                    description: product.description,
+                    color: colors.value,
+                    quantity: quantity.value
+                };
+                console.log(articlesPanier);
+                console.log(save.id)
 
-                localStorage.setItem("panier", JSON.stringify(articlesPanier));
-                // localStorage.setItem("id", product._id)
-                // localStorage.setItem("imageUrl", product.imageUrl)
-                // localStorage.setItem("altTxt", product.altTxt)
-                // localStorage.setItem("price", product.price)
-                // localStorage.setItem("description", product.description)
-                // localStorage.setItem("color", colors.value)
-                // localStorage.setItem("quantity", quantity.value)
-                console.log(JSON.parse(localStorage.panier));
+
+                //Envoie les données dans le local storage + change les quantité si la couleur et l'id est la même
+                if (typeof localStorage != 'undefined' && localStorage.getItem("panier") != null) {
+                    articlesPanier = JSON.parse(localStorage.getItem("panier"));
+                    const findProduct = articlesPanier.find((product) =>
+                        save.id === product.id && save.color === product.color
+
+                    )
+                    if (findProduct) {
+                        findProduct.quantity = quantity.value;
+
+                        localStorage.setItem("panier", JSON.stringify(articlesPanier))
+                        // et j'informe le clients de la quantité de produits dans sont panier
+                        document.querySelector(".item__content__settings__quantity").appendChild(document.createElement("p")).textContent =
+                        `Vous avez ${numberKanap} ${save.name} ${save.color} dans le pannier`
+
+                    } else {
+                        //Et si le produit existe pas je le push dans le tableau
+                        articlesPanier.push(save)
+                        localStorage.setItem("panier", JSON.stringify(articlesPanier))
+                        document.querySelector(".item__content__settings__quantity").appendChild(document.createElement("p")).textContent =
+                        `Vous avez ${numberKanap} ${save.name} ${save.color} dans le pannier`
+                    }
+                } else {
+                    // Permet de crée le premier produit
+                    articlesPanier.push(save)
+                    localStorage.setItem("panier", JSON.stringify(articlesPanier))
+                    document.querySelector(".item__content__settings__quantity").appendChild(document.createElement("p")).textContent =
+                        `Vous avez ${numberKanap} ${save.name} ${save.color} dans le pannier`
+                }
             }
+            let pastille = document.querySelector("nav ul").appendChild(document.createElement("span"));
+            pastille.textContent = "15";
+            pastille.style.fontSize = "12px"
+            pastille.style.color = "black";
+            pastille.style.border = "1px solid black";
+            pastille.style.borderRadius = "15px";
+            pastille.style.marginLeft = "10px"
+            pastille.style.display = "flex";
+            pastille.style.alignItems = "center";
+            pastille.style.justifyContent = "center";
+            pastille.style.alignSelf = "center"
+            pastille.style.height = "20px";
+            pastille.style.width = "20px";
+
+            // console.log(JSON.parse(localStorage.panier))
         }
+
     }
     );
 
